238d551a34fa0effad83bad15a55121c541ffdff
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-09-07 08:13:37 +0100

    init

---
+++ js/scrabble.js
@@ -0,0 +1,184 @@
+let wordshard = [
+  {
+    word: "fragile",
+    hint: "That can be easily broken",
+  },
+  {
+    word: "memorize",
+    hint: "To learn something by heart",
+  },
+  {
+    word: "miser",
+    hint: "One that hardly spends money",
+  },
+  {
+    word: "legible",
+    hint: "That can be read",
+  },
+  {
+    word: "recite",
+    hint: "To quote somethimg off hand",
+  },
+  {
+    word: "immortal",
+    hint: "Living forever (never dying)",
+  },
+  {
+    word: "biennial",
+    hint: "Occuring once in two years",
+  },
+  {
+    word: "biannual",
+    hint: "Occurng twice in a year",
+  },
+  {
+    word: "precise",
+    hint: "Brief, and to the point",
+  },
+  {
+    word: "edible",
+    hint: "That is fit for food",
+  },
+  {
+    word: "legitimate",
+    hint: "One that is born of married parents",
+  },
+  {
+    word: "incredible",
+    hint: "Difficult to believe",
+  },
+  {
+    word: "visible",
+    hint: "That which can be seen",
+  },
+  {
+    word: "literate",
+    hint: "Ability to read",
+  },
+  {
+    word: "obscure",
+    hint: "Not easily or clearly seen or understood",
+  },
+];
+
+let wordsmedium = [
+  {
+    word: "comfort",
+    hint: "A pleasant feeling of relaxation",
+  },
+  {
+    word: "tongue",
+    hint: "The muscular organ of mouth",
+  },
+  {
+    word: "expansion",
+    hint: "The process of increase or grow",
+  },
+  {
+    word: "country",
+    hint: "A politically identified region",
+  },
+  {
+    word: "recipe",
+    hint: "A set of instructions for preparing a food dish",
+  },
+  {
+    word: "anniversary",
+    hint: "An event that occurs on a fixed date every year",
+  },
+  {
+    word: "eligible",
+    hint: "Qualified to be chosen",
+  },
+  {
+    word: "aggressive",
+    hint: "Easily annoyed",
+  },
+  {
+    word: "queue",
+    hint: "A line of people waiting for something",
+  },
+  {
+    word: "meeting",
+    hint: "Event in which people come together",
+  },
+  {
+    word: "exchange",
+    hint: "The act of trading",
+  },
+  {
+    word: "canvas",
+    hint: "Piece of fabric for oil painting",
+  },
+  {
+    word: "garden",
+    hint: "Space for planting flower and plant",
+  },
+  {
+    word: "position",
+    hint: "Location of someone or something",
+  },
+  {
+    word: "feather",
+    hint: "Hair like outer covering of bird",
+  },
+];
+
+let wordseasy = [
+  {
+    word: "tragedy",
+    hint: "An event full of sorrow",
+  },
+  {
+    word: "number",
+    hint: "Math symbol used for counting",
+  },
+  {
+    word: "comedy",
+    hint: "An event full of jokes",
+  },
+  {
+    word: "group",
+    hint: "A number of objects or persons",
+  },
+  {
+    word: "taste",
+    hint: "Ability of tongue to detect flavour",
+  },
+  {
+    word: "store",
+    hint: "Large shop where goods are traded",
+  },
+  {
+    word: "field",
+    hint: "Area of land for farming activities",
+  },
+  {
+    word: "friend",
+    hint: "Person other than a family member",
+  },
+  {
+    word: "pocket",
+    hint: "A bag for carrying small items",
+  },
+  {
+    word: "needle",
+    hint: "A thin and sharp metal pin",
+  },
+  {
+    word: "expert",
+    hint: "Person with extensive knowledge",
+  },
+  {
+    word: "statement",
+    hint: "A declaration of something",
+  },
+  {
+    word: "second",
+    hint: "One-sixtieth of a minute",
+  },
+  {
+    word: "library",
+    hint: "Place containing collection of books",
+  },
+];
---
+++ js/scripthard.js
@@ -0,0 +1,48 @@
+const word = document.querySelector(".word"),
+hintTxt = document.querySelector(".hint span"),
+time = document.querySelector(".time b"),
+field = document.querySelector("input"),
+attemptBtn = document.querySelector(".another-attempt"),
+answerBtn = document.querySelector(".check-answer");
+
+let correctWord, timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0) {
+            maxTime--;
+            return time.innerText = maxTime;
+        }
+        alert(`Time up! ${correctWord.toUpperCase()} was the correct answer`);
+        initGame();
+    }, 1000);
+}
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = wordshard[Math.floor(Math.random() * wordshard.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    word.innerText = wordArray.join("");
+    hintTxt.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();;
+    field.value = "";
+    field.setAttribute("maxlength", correctWord.length);
+}
+initGame();
+
+const checkWord = () => {
+    let userWord = field.value.toLowerCase();
+    if(!userWord) return alert("Please enter your answer to check!");
+    if(userWord !== correctWord) return alert(`Oops! ${userWord} is not the correct answer`);
+    alert(`Hurray! ${correctWord.toUpperCase()} is the correct answer`);
+    initGame();
+}
+
+attemptBtn.addEventListener("click", initGame);
+answerBtn.addEventListener("click", checkWord);
+
